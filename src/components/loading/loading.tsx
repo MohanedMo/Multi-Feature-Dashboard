a776825f2718c7b0bdfff5842fdd72a0c331c80e
Mohaned Mohamed <mmhnd951@gmail.com>
2025-06-26 19:53:48 +0300

    Make loading, likes components and setup iconify library

---
+++ src/components/loading/loading.tsx
@@ -0,0 +1,25 @@
+import "./loading.css"
+import { Icon } from "@iconify/react";
+
+const LoadingComponent = () => {
+    return ( 
+        <div className="container">
+        <div className="card">
+          <div className="card-header">
+            <Icon icon="flat-color-icons:like" />
+            <h1 className="card-title">Blog Posts Dashboard</h1>
+          </div>
+          <div className="card-content">
+            <div className="skeleton skeleton-input"></div>
+            <div style={{ marginTop: "1rem" }}>
+              {Array.from({ length: 5 }).map((_, i) => (
+                <div key={i} className="skeleton skeleton-row"></div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+     );
+}
+ 
+export default LoadingComponent;
---
+++ src/components/loves/loves.tsx
@@ -0,0 +1,13 @@
+import { Icon } from "@iconify/react";
+import "./loves.css"
+
+const Loves = () => {
+    return ( 
+        <div className="loves-container">
+            <Icon icon="flat-color-icons:like" />
+            <span>2214</span>
+        </div>
+     );
+}
+ 
+export default Loves;
