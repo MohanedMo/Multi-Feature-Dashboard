0b2c556a9c0b25850855a8da9ca839e6fe4db19f
Mohaned Mohamed <mmhnd951@gmail.com>
2025-06-28 10:48:28 +0300

    Remove current project

--- posts/src/utils/helper-functions.tsx
+++
@@ -1,6 +0,0 @@
-let debounceTimer: number | undefined;
-export const debounce = (callback: () => void, durationInMs: number = 150) => {
-    if (!callback) return;
-    if (debounceTimer) clearTimeout(debounceTimer);
-    debounceTimer = setTimeout(callback, durationInMs);
-};
--- todo/src/App.jsx
+++
@@ -1,8 +0,0 @@
-// Components
-import TodoList from "./components/todo-list/todo-list";
-
-function App() {
-  return <TodoList />;
-}
-
-export default App;
